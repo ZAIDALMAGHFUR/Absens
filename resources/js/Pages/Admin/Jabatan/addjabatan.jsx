010c2ffd941073b7cc82ed09d0f126a7374243f0
Zaid al maghfur <91768583+ZAIDALMAGHFUR@users.noreply.github.com>
2023-02-04 23:40:12 +0700

    finall sweetalert2

--- resources/js/Pages/Admin/Jabatan/addjabatan.jsx
+++ resources/js/Pages/Admin/Jabatan/addjabatan.jsx
@@ -12,21 +12,30 @@ export default function addJabatan(props) {
     const handleSubmit = (e) => {
         e.preventDefault();
 
+        function showSuccessAlert() {
+            Swal.fire({
+                title: 'Success!',
+                text: 'Data saved successfully!',
+                icon: 'success',
+                showConfirmButton: true,
+                timer: 15000,
+            });
+        }
+
         Inertia.post('/storejabatan', {
             name: name
         }, {
-            onSuccess: () => {
-
-                //show alert
+            onSuccess: showSuccessAlert,
+            onError: (errors) => {
                 Swal.fire({
-                    title: 'Success!',
-                    text: 'Data saved successfully!',
-                    icon: 'success',
+                    title: 'Error!',
+                    text: 'Position  already exists !',
+                    icon: 'error',
                     showConfirmButton: true,
-                    timer: 15000
-                })
-            }
-        })
+                    timer: 15000,
+                });
+            },
+        });
     }
 
 
@@ -49,11 +58,11 @@ export default function addJabatan(props) {
                                 <label type="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name Position</label>
                                 <input type="name" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Manager" onChange={(name) => setName(name.target.value)} value={name} />
                             </div>
-                            {errors.name && (
+                            {/* {errors.name && (
                                 <div className="alert bg-red-600">
                                     {errors.name}
                                 </div>
-                            )}
+                            )} */}
                             <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={() => handleSubmit()}>Add Position</button>
                         </form>
                     </div>
--- resources/js/Pages/Admin/Jabatan/editjabatan.jsx
+++ resources/js/Pages/Admin/Jabatan/editjabatan.jsx
@@ -14,6 +14,17 @@ export default function editjabatan(props) {
     //method "updateCategory"
     const updatePosition = async (e) => {
         e.preventDefault();
+
+        function showSuccessAlert() {
+            Swal.fire({
+                title: 'Success!',
+                text: 'Data saved successfully!',
+                icon: 'success',
+                showConfirmButton: true,
+                timer: 15000,
+            });
+        }
+
         //sending data
         Inertia.post(`/updatejabatan/${props.alljabatan.id}`, {
 
@@ -21,16 +32,15 @@ export default function editjabatan(props) {
             name: name,
             _method: "PUT"
         }, {
-            onSuccess: () => {
-
-                //show alert
+            onSuccess: showSuccessAlert,
+            onError: (errors) => {
                 Swal.fire({
-                    title: 'Success!',
-                    text: 'Data updated successfully!',
-                    icon: 'success',
-                    showConfirmButton: false,
-                    timer: 1500
-                })
+                    title: 'Error!',
+                    text: 'Data failed to Update!',
+                    icon: 'error',
+                    showConfirmButton: true,
+                    timer: 15000,
+                });
             }
         });
     }
@@ -52,11 +62,11 @@ export default function editjabatan(props) {
                                 <label type="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name Position</label>
                                 <input type="name" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Manager" value={name} onChange={(e) => setName(e.target.value)} />
                             </div>
-                            {errors.name && (
+                            {/* {errors.name && (
                                 <div className="alert bg-red-600">
                                     {errors.name}
                                 </div>
-                            )}
+                            )} */}
                             <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update Position</button>
                         </form>
                     </div>
